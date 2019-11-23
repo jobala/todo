5d4727293ea550f1d051f723ea553231148cb68f
jobala <japhethobalak@gmail.com>
2019-11-23 10:28:00 +0300

    Hello World in assembly

--- assembly/todo.s
+++ assembly/todo.s
@@ -1,5 +1,16 @@
-section .data
+; Executable name					:	todo
+; Version 								: 1.0
+; Description							: A simple todo application that helps users keep track of what they should do
+;
+;	Run it this way:
+; ./todo
+;
+; Build using this command
+; make todo
 
+section .data
+	Msg: db "Hello, World", 10 								; Create the message variable
+	MsgLen equ $- Msg													; Calculate the length of the message
 
 section .bss
 
@@ -7,5 +18,16 @@ section .bss
 section .text
 	global _start
 
-start:
-; Start of the program
+_start:
+	call say_hello
+
+
+say_hello:
+	mov eax, 4																; Specify the sys_write system call
+	mov ebx, 1																; Selects the file descriptor to write to in this case it's stdout
+	mov ecx, Msg															; Msg to write
+	mov edx, MsgLen														; The computer doesn't know how big the message is let it know
+	int 0x80																	; Make the system call
+
+	mov eax, 1																; Specify the sys_exit system call
+	int 0x80																	; Make the system calll
