56fbf837e3b22b2acc5195788e6301a3460a3877
jobala <japhethobalak@gmail.com>
2019-11-23 19:25:55 +0300

    Fixes bug where .txt file was not getting created

--- assembly/todo.s
+++ assembly/todo.s
@@ -11,32 +11,44 @@
 section .data
 	Msg: db "Welcome to the assembly todo manager", 10, "------------------------------------",10								; Create the message variable
 	MsgLen equ $- Msg																									; Calculate the length of the message
-	FileName: db "database.txt"																				; File name for the file storing todo items
+	FileName: db "databasee.txt"																				; File name for the file storing todo items
 	AccessPermissions equ 0777																				; Read, write & execute by all
 
 section .bss
-
-
+	fd_out resb 1
+	
 section .text
 	global _start
 
 _start:
 	call welcome_message
 	call open_or_create_file
+	; call write_to_file
+
 
 
 open_or_create_file:
-	pushad																		; Push the callers general purpose registers into the stack
 	mov eax, 8																; Specifies sys_creat syscall
 	mov ebx, FileName													; Provide the filename
 	mov ecx, AccessPermissions							  ; Specify the permissions with which the file should be created/opened
-	popad																			; Pop the callers general purpose registers from the stack
 
 	int 0x80																	; make the syscall
 	ret
 
-read_file:
+; write_to_file:
+; 	pushad																		; Push the caller's general purpose registers into the stack
+; 	mov eax, 4																; Specify the sys_write syscall
+; 	mov ebx, [fd_out]													; Set the file descriptor
+; 	mov ecx, Msg
+; 	popad																			; Pop the caller's general purpose registers into the stack
 
+; 	int 0x80
+
+; 	; close the file
+; 	mov eax, 6																; Specify sys_close syscall
+; 	int 0x80
+
+; 	ret
 
 welcome_message:
 	mov eax, 4																; Specify the sys_write system call
@@ -45,5 +57,4 @@ welcome_message:
 	mov edx, MsgLen														; The computer doesn't know how big the message is let it know
 	int 0x80																	; Make the system call
 
-	mov eax, 1																; Specify the sys_exit system call
-	int 0x80																	; Make the system calll
+	ret
